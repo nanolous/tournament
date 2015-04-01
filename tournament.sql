476dafa66c588de3d14e33d7f8ccf6eb5985fa85
nanolous <ziolndr@gmail.com>
2015-03-31 21:45:27 -0700

    comment SQL schema

--- tournament.sql
+++ tournament.sql
@@ -6,24 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+-- PLayers table, where the primary key is defined, and name field.
 CREATE TABLE PLAYERS (
 ID SERIAL PRIMARY KEY,
 Name TEXT);
 
+-- Matches table, unique serial ID, and winner/loser columns that reference the primary key in players table.
 CREATE TABLE MATCHES (
 ID SERIAL,
 Winner INTEGER references PLAYERS(ID),
 Loser INTEGER references PLAYERS(ID));
 
-CREATE TABLE PLAYER_STANDINGS (
-ID SERIAL,
-Player1 INTEGER,
-Player2 INTEGER);
-
+-- Standings view, aggregates matches data per player with fields specified from players table, and a join from the matches table.
 CREATE VIEW AS STANDINGS 
 SELECT players.id, players.name,COUNT(matches.winner) AS wins, 
 COUNT(DISTINCT matches.loser) + COUNT(matches.winner) AS matches 
 FROM players LEFT JOIN matches ON players.id = matches.winner 
 GROUP BY players.id ORDER BY wins DESC;
-
